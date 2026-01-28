3dbd9ed1f20fae4c956ca424cea152be3571ea51
Hayden-21 <hayden.hardy@student.atlantaclassical.org>
2026-01-28 15:11:09 -0500

    Update test.c with new function definitions
    Refactor functions and fix syntax errors in test.c

--- test.c
+++ test.c
@@ -1,26 +1,14 @@
 #include <stdio.h>
-void print_0_10()
+void aray()
 {
-  for(int i=0; i<=10;i++)
-    {printf("%d\n",i);}
-
+  int array (85);
 }
-int main()
-{print_0_10();
-}
-void print_2_20()
-{
-  for(int i=2; i<=20;i++)
-{printf("%d/n",i);}
-}
-{print_2_20();}
-void print_4_40()
-{
-  for(int=4; i<=40;i+=2)
-{printf("%d/n",i),;}
-}
-void print_101_303()
-{
-  for(int i=101; i<=303;i+=1)
-{printf(%d/n",i);
+void array(){
+  int array (20)(20)
+    }
+void populatearray() {
+  for {int i=0 i<=84; i++} {
+int array[i];
   }
+}
+void populatethearray()
