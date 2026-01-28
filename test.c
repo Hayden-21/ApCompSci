57f86aa576a1b436e1ae5f7042bef6a401d1f533
Hayden-21 <hayden.hardy@student.atlantaclassical.org>
2026-01-28 14:52:24 -0500

    Implement multiple print functions in test.c
    Added functions to print numbers in various ranges.

--- test.c
+++ test.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
-
-int main() {
-
-}
-int[50] build_arr()
+void print_0_10()
 {
-  int arr[50];
-  for(int i=0; i < 50; i++)
-    {
-      arr[i] = (i+1)*2;
+  for(int i=0; i<=10;i++)
+    {printf("%d\n",i);}
+
 }
-int search_arr(){
+int main()
+{print_0_10();
 }
+void print_2_20()
+{
+  for(int i=2; i<=20;i++)
+{printf("%d/n",i);}
+}
+{print_2_20();}
+void print_4_40()
+{
+  for(int=4; i<=40;i+=2)
+{printf("%d/n",i),;}
+}
+void print_101_303()
+{
+  for(int i=101; i<=303;i+=1)
+{printf(%d/n",i);
+  }
