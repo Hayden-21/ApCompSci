5967d70ce6f143959197a69e5b77d23bd566d5ff
Hayden-21 <hayden.hardy@student.atlantaclassical.org>
2026-02-12 15:11:08 -0500

    Fix syntax errors and improve print statements

--- number_game.py
+++ number_game.py
@@ -2,8 +2,10 @@ import random
 num=random.randint(1, 999)
 while True
  guess = int(input("Enter your guess: "))
-             if guess < num
-             printf("Number to low")
+    if guess < num
+             print(f"Number to low")
              if else guess > num
-             printf(" Number to high")
-else printf("Your corect")
+             print(f" Number to high")
+            else print(f"Your corect")
+break
+
