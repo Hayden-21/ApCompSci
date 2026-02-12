ff6f4d0a6ef92903cff97c96e3354595e121b990
Hayden-21 <hayden.hardy@student.atlantaclassical.org>
2026-02-12 15:02:58 -0500

    Correct number generation and guessing logic
    Fix number guessing game logic and syntax errors.

--- number_game.py
+++ number_game.py
@@ -1,2 +1,9 @@
 import random
-num=random. 
+num=random.randint(1, 100)
+while True;
+ guess = int(input("Enter your guess: ")
+             if guess < num
+             printf("Number to low")
+             if else guess > num
+             printf(" Number to high")
+else printf("Your corect")
