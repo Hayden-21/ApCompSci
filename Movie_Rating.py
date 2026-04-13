447a7e1d711128655fb8450e40f41754a80dcd16
Hayden-21 <hayden.hardy@student.atlantaclassical.org>
2026-04-13 15:19:53 -0400

    Initialize Movie Rating App with welcome message
    Added a welcome message and started movie input function.

--- Movie_Rating.py
+++ Movie_Rating.py
@@ -1,1 +1,8 @@
-
+print "Welcome to Movie Rating App"
+Movies = []
+def movie_input():
+def eval_movie_input():"What is the title of the movie?"
+   if movie == "done" "Done" "DONE"
+     return "Movie list complete"
+    movie = movie.appends
+   
