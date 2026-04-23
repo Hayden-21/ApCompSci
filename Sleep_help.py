0062983fedc24de9e87d49826ab76852a410b00e
Hayden-21 <hayden.hardy@student.atlantaclassical.org>
2026-04-23 14:59:36 -0400

    Refactor greeting and improve sleep input logic
    Refactor greeting into a function and fix input handling.

--- Sleep_help.py
+++ Sleep_help.py
@@ -1,7 +1,15 @@
-print"Welcome, I hope you slept"
+def say_greeting():
+  print("Welcome, I hope you slept")
+say_greeting()
 day = []
   weekday = input("What day of sleep are we helping?")
   day.appends(weekday)
   sleep = intinput("How did you sleep 1-10")
-    
-  
+  if sleep == 9 or 10 
+    return "You slept incredibly keep it up"
+  elif sleep == 7 or 8
+    return "focus on enhancing sleep hygiene,optimizing the bedroom enviorment, and managing late-night activities. Finally maintain a strict and consistent sleep schedule"
+  elif sleep == 6 
+    return "Establish a 30-60min routine before bed, avoid screens, caffiene, and alcohol. Optimize your bedroom to be dark cool and quiet"
+  elif sleep == 5 
+      return "
