d6d3e113f0ed8ede912e02910a8aff077c71ed34
Hayden-21 <hayden.hardy@student.atlantaclassical.org>
2026-04-23 14:36:49 -0400

    Correct input function for sleep rating
    Fixed input function for sleep rating to convert to integer.

--- Sleep_help.py
+++ Sleep_help.py
@@ -2,6 +2,6 @@ print"Welcome, I hope you slept"
 day = []
   weekday = input("What day of sleep are we helping?")
   day.appends(weekday)
-  sleep = input("How did you sleep 1-10")
+  sleep = intinput("How did you sleep 1-10")
     
   
