a7ef909be7afb750cd22c520189b0fd73c4b1957
Hayden-21 <hayden.hardy@student.atlantaclassical.org>
2026-04-30 14:42:59 -0400

    Change return statements to print statements

--- Sleep_help.py
+++ Sleep_help.py
@@ -6,20 +6,20 @@ weekday = input("What day of sleep are we helping?")
   day.append(weekday)
   sleep = int(input("How did you sleep 1-10"))
   if sleep == 9 or sleep == 10:
-    return "You slept incredibly keep it up"
+    print("You slept incredibly keep it up")
   elif sleep == 7 or 8:
-    return "focus on enhancing sleep hygiene,optimizing the bedroom enviorment, and managing late-night activities. Finally maintain a strict and consistent sleep schedule"
+     print("focus on enhancing sleep hygiene,optimizing the bedroom enviorment, and managing late-night activities. Finally maintain a strict and consistent sleep schedule")
   elif sleep == 6:
-    return "Establish a 30-60min routine before bed, avoid screens, caffiene, and alcohol. Optimize your bedroom to be dark cool and quiet"
+    print("Establish a 30-60min routine before bed, avoid screens, caffiene, and alcohol. Optimize your bedroom to be dark cool and quiet")
   elif sleep == 5: 
-      return "You should include physical activity throughout the day,If you haven't slept then get up and do a quiet and slow cool down routine before bed"
+      print("You should include physical activity throughout the day,If you haven't slept then get up and do a quiet and slow cool down routine before bed")
   elif sleep == 4:
-      return "Get sunlight in the morning and avoid phones, Avoid long naps in the afternoon, try low-dose melatonin"
+      print("Get sunlight in the morning and avoid phones, Avoid long naps in the afternoon, try low-dose melatonin")
   elif sleep == 3:
-      return "Remove screens an hour before bed, Keep naps under 30 minutes, Write down your worries, practice mindfulness/meditation to reduce anxiety"
+      print("Remove screens an hour before bed, Keep naps under 30 minutes, Write down your worries, practice mindfulness/meditation to reduce anxiety")
   elif sleep == 2:
-      return " Prevent any phone use 1hr before bed, practice deep breathing, don't eat heavy meals"
+      print(" Prevent any phone use 1hr before bed, practice deep breathing, don't eat heavy meals")
   elif sleep == 1:
-      return "Do stretching and reading 20 minutes before bed,Stay active throughout the day, Report to a doctor for continued issues"
-  else
-      return "Invalid Response"
+      print("Do stretching and reading 20 minutes before bed,Stay active throughout the day, Report to a doctor for continued issues")
+  else;
+      print("Invalid Response")
